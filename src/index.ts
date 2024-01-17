d547453967023433d021378ba416dea0faf3322a
anic4cio <anicaciovinicius36@gmail.com>
2024-01-17 17:54:02 -0300

    refactor: index as a index

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,10 @@
 import { 
-  ISlackFileListParams,
-  getTextFromSlackFile,
-  getListOfSlackFiles,
-  IRequestFile
+  getTextOfLastFileFromSlack
 } from "./slackFileGetter"
 
-const channelId = process.env.CHANNEL_ID!
-const slackUser = process.env.SLACK_USER!
 
 ;(async () => {
-  const now = new Date().getTime().toString()
-  const reqArgs: ISlackFileListParams = {
-    channel: channelId,
-    count: 1,
-    show_files_hidden_by_limit: false,
-    ts_from: now,
-    types: 'pdfs',
-    user: slackUser
-  }
-
-  const response = await getListOfSlackFiles(reqArgs)
-  const filename = response[0].name
-  const downloadFileUrl = response[0].url_private_download
-
-  const slackFile: IRequestFile = {
-    files: [{
-      url_private_download: downloadFileUrl,
-      name: filename,
-    }]
-  }
-
-  const text = await getTextFromSlackFile(slackFile)
+  const text = await getTextOfLastFileFromSlack()
   console.log(text)
 })()
 
