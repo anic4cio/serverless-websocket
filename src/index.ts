73345d742e7a91e4c3e94933e89d7d69b0dd762f
anic4cio <anicaciovinicius36@gmail.com>
2024-01-19 18:21:05 -0300

    chore: shotcut request body

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,57 @@
-import { 
-  getTextOfLastFileFromSlack
+import {
+  sendMessageToSlack,
+  sendJSONToSlack,
+  getTextFromSlackFile
 } from "./slackFileGetter"
+import { http } from '@google-cloud/functions-framework'
+import { Request, Response } from 'express'
 
-;(async () => {
-  await getTextOfLastFileFromSlack()
-})()
+interface ISlackRequest {
+  message_ts: string
+  message: IMessage
+  user: IUser
+}
+
+interface IMessage {
+  text: string
+  file: {
+    filetype: string
+    url_private_download: string
+  }[]
+}
+
+interface IUser {
+  id: string
+  username: string
+  name: string
+}
+
+http('start', async (req: Request, res: Response) => {
+  const payload = req.body.payload
+  
+  try {
+    console.log(payload)
+  } catch (error) {
+    console.error(error)
+  }
+
+  try {
+    console.log(payload.action_ts)
+  } catch (error) {
+    console.error(error)
+  }
+
+  try {
+    console.log(payload.message)
+  } catch (error) {
+    console.error(error)
+  }
+
+  try {
+    console.log(payload.message_ts)
+  } catch (error) {
+    console.error(error)
+  }
+
+  res.send('OK!')
+})
